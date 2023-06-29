2c1f345d98dca9cca088c6b9b2a4dfea5aec270a
Mauro Fetingis <maurofetingis17@gmail.com>
2023-06-29 11:14:49 -0300

    otro commit

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
-const stockProductos = [
-    {id: 1, class: "articulo", nombre: "Comida para perro grande", cantidad: 1, desc: "Comida balanceada tamaño grande", precio: 1200, img:'./img/comidaPG.jpg'},
-    {id: 2, class: "articulo", nombre: "Comida para perro pequeña", cantidad: 1, desc: "Comida balanceada tamaño pequeña", precio: 1100, img: './img/comidaPC.jpg'},
-    {id: 3, class: "articulo", nombre: "Comida para gato grande", cantidad: 1, desc: "Comida balanceada tamaño grande", precio: 1200, img: './img/comidaG.jpg'},
-    {id: 4, class: "articulo", nombre: "Comida para gato pequeña",cantidad: 1, desc: "Comida balanceada tamaño pequeña", precio: 1000, img: './img/comidaG.jpg'},
-    {id: 5, class: "articulo", nombre: "Correa para perro", cantidad: 1, desc: "Correa reajustable para perros", precio: 1200, img: './img/correa.jpg'},
-    {id: 6, class: "articulo", nombre: "Premio para mascotas", cantidad: 1, desc: "Un bocadillo ideal para tu mascota", precio: 900, img: './img/premios.jpg'},
-    {id: 7, class: "articulo", nombre: "Arena para gato", cantidad: 1, desc: "Arena hipoalergenica para gatos", precio: 500, img: './img/arenaGato.png'},
-    {id: 8, class: "articulo", nombre: "Collar para gato", cantidad: 1, desc: "Collar reajustable para gatos", precio: 500, img: './img/collarG.png'},
-    {id: 9, class: "articulo", nombre: "Juguete para perro",cantidad: 1, desc: "Juguete duradero para perros en variedad de colores", precio: 200, img: './img/JugueteP.png'},
-    {id: 10, class: "articulo", nombre: "Juguete para gato",cantidad: 1, desc: "Juguete super esponjoso para gatos con ruidos ", precio: 200, img: './img/jugueteG.jpg'},
-    {id: 11, class: "articulo", nombre: "Cama para perros", cantidad: 1,desc: "Cama acolchonada para interior o exterior", precio: 2500, img: './img/camaMascota.png'},
-    {id: 12, class: "articulo", nombre: "Cama para gatos", cantidad: 1, desc: "Cama acolchonada para gatos con manta incluida", precio: 2200, img: './img/camaMascota.png'},
-    {id: 13, class: "articulo", nombre: "Ropa para perro", cantidad: 1, desc: "Ropa abrigada de invierno para perros", precio: 1000, img: './img/ropaP.jpg'},
-    {id: 15, class: "articulo", nombre: "Champu para animales", cantidad: 1, desc: "Champú antipulgas para animales", precio: 1000, img: './img/champu.png'},
-    {id: 16, class: "articulo", nombre: "Peine para animales", cantidad: 1, desc: "Peine con cerdas suaves para animales", precio: 700, img: './img/peine.png'},
+// const stockProductos = [
+//     {id: 1, class: "articulo", nombre: "Comida para perro grande", cantidad: 1, desc: "Comida balanceada tamaño grande", precio: 1200, img:"./img/comidaPG.jpg"},
+//     {id: 2, class: "articulo", nombre: "Comida para perro pequeña", cantidad: 1, desc: "Comida balanceada tamaño pequeña", precio: 1100, img: "./img/comidaPC.jpg"},
+//     {id: 3, class: "articulo", nombre: "Comida para gato grande", cantidad: 1, desc: "Comida balanceada tamaño grande", precio: 1200, img: "./img/comidaG.jpg"},
+//     {id: 4, class: "articulo", nombre: "Comida para gato pequeña",cantidad: 1, desc: "Comida balanceada tamaño pequeña", precio: 1000, img: "./img/comidaG.jpg"},
+//     {id: 5, class: "articulo", nombre: "Correa para perro", cantidad: 1, desc: "Correa reajustable para perros", precio: 1200, img: "./img/correa.jpg"},
+//     {id: 6, class: "articulo", nombre: "Premio para mascotas", cantidad: 1, desc: "Un bocadillo ideal para tu mascota", precio: 900, img: "./img/premios.jpg"},
+//     {id: 7, class: "articulo", nombre: "Arena para gato", cantidad: 1, desc: "Arena hipoalergenica para gatos", precio: 500, img: "./img/arenaGato.png"},
+//     {id: 8, class: "articulo", nombre: "Collar para gato", cantidad: 1, desc: "Collar reajustable para gatos", precio: 500, img: "./img/collarG.png"},
+//     {id: 9, class: "articulo", nombre: "Juguete para perro",cantidad: 1, desc: "Juguete duradero para perros en variedad de colores", precio: 200, img: "./img/JugueteP.png"},
+//     {id: 10, class: "articulo", nombre: "Juguete para gato",cantidad: 1, desc: "Juguete super esponjoso para gatos con ruidos ", precio: 200, img: "./img/jugueteG.jpg"},
+//     {id: 11, class: "articulo", nombre: "Cama para perros", cantidad: 1,desc: "Cama acolchonada para interior o exterior", precio: 2500, img: "./img/camaMascota.png"},
+//     {id: 12, class: "articulo", nombre: "Cama para gatos", cantidad: 1, desc: "Cama acolchonada para gatos con manta incluida", precio: 2200, img: "./img/camaMascota.png"},
+//     {id: 13, class: "articulo", nombre: "Ropa para perro", cantidad: 1, desc: "Ropa abrigada de invierno para perros", precio: 1000, img: "./img/ropaP.jpg"},
+//     {id: 15, class: "articulo", nombre: "Champu para animales", cantidad: 1, desc: "Champú antipulgas para animales", precio: 1000, img: "./img/champu.png"},
+//     {id: 16, class: "articulo", nombre: "Peine para animales", cantidad: 1, desc: "Peine con cerdas suaves para animales", precio: 700, img: "./img/peine.png"},
     
-]
-//algo nuevo//
+// ]
+
+// algo nuevo//
+
+let stockProductos = []
+
+fetch("./products.json")
+.then(response => response.json())
+.then(data => (
+    stockProductos = data;
+    cargarProd(stockProductos);
+))
 
 
 
 
+function cargarProd(){
 
-
+}
 
 //termina algo nuevo//
 
-const contenedorProductos = document.getElementById('contenedor-productos')
-
+const contenedorProductos = document.getElementById('container-productos')
 
 const contenedorCarrito = document.getElementById('carrito-contenedor')
 
